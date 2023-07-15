f7b471de04cc8d8cdc0a5204813544ab95978371
shail sharma <shail.sharma@gunagya.in>
2023-07-15 15:58:47 +0530

    test

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function Square({ value, onSquareClick }) {
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
+    
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
